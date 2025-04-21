294893482444fa3a48f3078fd6774a6bd592fd8c
Matías Angélico <mnangelico@gmail.com>
2025-04-21 19:20:52 -0300

    cambio en matrices de clustering al entrar a la red

--- main.py
+++ main.py
@@ -168,73 +168,6 @@ async def clustering(x_api_key: str = Header(None), matrices_descriptores: Uploa
     return {"matrices_clustering": respuesta_matrices, "imagenes_clustering": respuesta_imagenes}
 
 
-@app.post("/entrenamiento")
-async def neuronal(background_tasks: BackgroundTasks, x_api_key: str = Header(None), matrices_descriptores: UploadFile = File(), matriz_clustering: UploadFile = File(), parametros_entrenamiento: str = Form(...)):
-    await validaApiKey(x_api_key)
-    desc_json = await matrices_descriptores.read()
-    matrices_desc = json.loads(desc_json)
-
-    clus_json = await matriz_clustering.read()
-    matriz_clus = json.loads(clus_json)
-
-    train_params = json.loads(parametros_entrenamiento)
-
-    total = len(matrices_desc)
-
-    print(f"Nro de matrices de descriptores recibidas: {total}")
-    print(f"Clustering seleccionado: {matriz_clus['id_clustering']}")
-
-    tensor = np.zeros((len(matrices_desc[0]['matriz_descriptor']), len(
-        matrices_desc[0]['matriz_descriptor'][0]), total))
-
-    for t, datos in enumerate(matrices_desc):
-        tensor[:, :, t] = np.array(datos['matriz_descriptor'])
-
-    entrada = tensor.reshape(-1, total)
-    resultados = np.array(matriz_clus['matriz_clustering']).reshape(-1)
-    nro_clusters = matriz_clus['nro_clusters']
-
-    print(entrada.shape)
-    print(resultados.shape)
-
-    layers = train_params['neuralNetworkLayers']
-
-    parametros_entrenamiento = np.zeros((len(layers), 3))
-
-    for t, datos in enumerate(layers):
-        parametros_entrenamiento[t, 0] = float(datos['neurons'])
-        parametros_entrenamiento[t, 1] = int(datos['batchNorm'])
-        parametros_entrenamiento[t, 2] = float(datos['dropout'])
-
-    params = train_params['neuralNetworkParams']
-    epocas = int(params['epocs'])
-    b_size = int(params['batchsize'])
-    estop = int(params['earlystopping'].lower() == 'true')
-
-    print(parametros_entrenamiento)
-
-    model, conf_matrix = train.entrenamientoRed(
-        entrada, resultados, nro_clusters, parametros_entrenamiento, epocas, b_size, estop)
-
-    model_path = "modelo_entrenado.keras"
-    model.save(model_path)
-
-    imagen_json = {"matriz_confusion": base64.b64encode(
-        conf_matrix.getvalue()).decode('utf-8')}
-    imagen_json_str = json.dumps(imagen_json)
-
-    zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
-        zip_file.write(model_path, arcname="modelo_entrenado.keras")
-        zip_file.writestr("matriz_confusion.json",
-                          imagen_json_str.encode('utf-8'))
-
-    zip_buffer.seek(0)
-
-    background_tasks.add_task(eliminar_archivo, model_path)
-
-    return StreamingResponse(zip_buffer, media_type="application/zip", headers={"Content-Disposition": "attachment; filename=archivos.zip"})
-
 @app.post("/entrenamientoRed")
 async def neuronal2(background_tasks: BackgroundTasks, x_api_key: str = Header(None), matrices_descriptores: UploadFile = File(), parametros_entrenamiento: str = Form(...)):
     await validaApiKey(x_api_key)
@@ -255,6 +188,7 @@ async def neuronal2(background_tasks: BackgroundTasks, x_api_key: str = Header(N
     matriz_caracteristicas[:,total-1] = np.array(matrices[total-1]['matriz_clustering'])
 
     nro_clusters = (np.unique(np.array(matrices[total-1]['matriz_clustering']))).size
+    matriz_caracteristicas[:,total-1] +=-1
     print ('nro de cluster ',nro_clusters)
     print(matriz_caracteristicas.shape)
 
@@ -299,48 +233,6 @@ async def eliminar_archivo(path: str):
     if os.path.exists(path):
         os.remove(path)
 
-@app.post("/prediccion")
-async def prediccion(background_tasks: BackgroundTasks, x_api_key: str = Header(None), modelo_entrenado: UploadFile = File(...), matrices_descriptores: UploadFile = File()):
-    await validaApiKey(x_api_key)
-    model_path = "modelo_temporal.keras"
-    
-    with open(model_path, "wb") as f:
-        f.write(await modelo_entrenado.read())
-
-    modelo = keras.models.load_model(model_path, custom_objects={'mse': metrics.MeanSquaredError()})
-
-    desc_json = await matrices_descriptores.read()
-    matrices_desc = json.loads(desc_json)
-
-    total = len(matrices_desc)
-    print(f"Nro de matrices de descriptores recibidas: {total}")
-
-    alto = len(matrices_desc[0]['matriz_descriptor'][0])
-    ancho = len(matrices_desc[0]['matriz_descriptor'][1])
-
-    tensor = np.zeros((alto, ancho, total))
-
-    for t, datos in enumerate(matrices_desc):
-        tensor[:, :, t] = np.array(datos['matriz_descriptor'])
-
-    entrada = tensor.reshape(-1, total)
-
-    print(entrada.shape)
-
-    resultado = modelo.predict(entrada).astype(np.float32)
-
-    resultado_matriz = np.argmax(resultado, axis=-1)
-
-    resultado_matriz = resultado_matriz.reshape(alto, ancho)
-
-    background_tasks.add_task(eliminar_archivo, model_path)
-
-    respuesta_matriz = {"matriz": resultado_matriz.tolist()}
-    respuesta_tensor = {"tensor": resultado.tolist()}
-    respuesta_imagen = {"imagen": generaImagenPrediccion.colorMap(resultado_matriz.tolist())}
-
-    return {"matriz_prediccion": respuesta_matriz, "imagen_prediccion": respuesta_imagen, "tensor_prediccion": respuesta_tensor}
-
 
 @app.post("/prediccionRed")
 async def prediccion(background_tasks: BackgroundTasks, x_api_key: str = Header(None), modelo_entrenado: UploadFile = File(...), matrices_descriptores: UploadFile = File(),video_dimensiones: str = Form(...)):
@@ -374,6 +266,8 @@ async def prediccion(background_tasks: BackgroundTasks, x_api_key: str = Header(
     resultado_matriz = np.argmax(resultado, axis=-1)
 
     resultado_matriz = resultado_matriz.reshape(alto,ancho)
+
+    resultado_matriz +=1
 
     background_tasks.add_task(eliminar_archivo, model_path)
 
